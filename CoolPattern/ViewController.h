a3e30263372937dd6b2af3b0c439f3870ea3a518
Arepu Pavankumar <iosdeveloper.ipa@gmail.com>
2016-02-21 23:51:20 +0530

    CoolPatterm - Git
    Nice Example for designing own views - Patterns  - Inherited from UIView

---
+++ CoolPattern/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CoolPattern
+//
+//  Created by Ray Wenderlich on 4/15/13.
+//  Copyright (c) 2013 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
+
+
